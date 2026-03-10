ed7418178d868b13f6d059816e447a02232893b5
nandhakishoreramanan <nandhakishoreramanan@gmail.com>
2026-03-10 22:14:01 +0530

    added background audio

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -3,6 +3,7 @@ import { NavLink, Outlet, useLocation } from 'react-router';
 import { motion, AnimatePresence } from 'motion/react';
 import { Trophy, Gamepad2, Settings, Orbit, Menu, X } from 'lucide-react';
 import { useTournament } from '../context/TournamentContext';
+import { BackgroundAudio } from './BackgroundAudio';
 
 const Particles = () => {
   return (
@@ -115,6 +116,9 @@ export function Layout() {
               </h1>
             </div>
 
+            <div className="flex items-center gap-2">
+              <BackgroundAudio />
+            </div>
           </div>
         </div>
 
